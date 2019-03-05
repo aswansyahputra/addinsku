a43cc072153d3967b7f88637563d152ecb7912ff
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-03-05 23:30:55 +0700

    Initial commit

---
+++ R/myaddins.R
@@ -0,0 +1,7 @@
+baca_r4ds <- function(){
+  utils::browseURL("https://r4ds.had.co.nz/")
+}
+
+baca_solusi_r4ds <- function(){
+  utils::browseURL("https://jrnold.github.io/r4ds-exercise-solutions/")
+}
